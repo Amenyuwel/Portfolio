f903110f8bd2a39535c12f55f7ea22dd018f06bc
Amenyuwel <Amenyuwel@gmail.com>
2025-07-18 11:57:26 +0800

    Added useclients to components.

--- app/pages/stacks/page.jsx
+++ app/pages/stacks/page.jsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import Stacks from "@/components/Stacks";
 import ScrollUp from "@/components/ScrollUp";
--- components/ProjectsGrid.jsx
+++ components/ProjectsGrid.jsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import { Icon } from "@iconify/react";
 import { projects } from "@/app/newProjects"; // Import projects data
